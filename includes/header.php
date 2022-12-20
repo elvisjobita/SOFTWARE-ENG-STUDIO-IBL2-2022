e8d2069dbacb4b2cb6b273c533e5d05b3bfd0d85
elvisjobita <elvieslovietoh@gmail.com>
2022-12-20 12:24:23 +0300

    Header and footer styles

--- includes/header.php
+++ includes/header.php
@@ -6,3 +6,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="description" content="An online store for school kids to shop school esentials" />
 
+<!-- ============ Google Lato Font ============ -->
+<link href="https://fonts.googleapis.com/css2?family=Lato:wght@400&display=swap" rel="stylesheet">
+
+<!-- ============ Font Awesom ============= -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+
+<!-- ============ Bootstrap CSS ============ -->
+<link rel="stylesheet" href="https://res.cloudinary.com/phynoinc/raw/upload/v1576137886/assets/styles/consmo.css">
+
+<!-- ============ Custom CSS ============ -->
+<link rel="stylesheet" href=<?php echo BASE_URL . "static/css/styles.css" ?>>
